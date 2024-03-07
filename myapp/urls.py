c5521352e8587fc3850768b24efe403a54a43956
MIGUEVARVAL2 <mivargasv@unal.edu.co>
2024-03-07 11:39:27 -0500

    Se agrega el perfil del profesor y la navegacion con el servidor

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('index/', index),
+    path('index/', views.index,name="index"),
+    path('cursos_estudiantes/', views.cursos_estudiantes,name="cursos_estudiantes"),
+    path('informacion_curso/', views.informacion_curso,name="informacion_curso"),
+    path('listar_estudiantes_curso/', views.listar_estudiantes_curso,name="listar_estudiantes_curso"),
+    path('tomar_asistencia/', views.tomar_asistencia,name="tomar_asistencia"),
+    path('perfil_profesor/', views.perfil_profesor,name="perfil_profesor"),
 ]
